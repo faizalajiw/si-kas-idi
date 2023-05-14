fa2f14648b0e9710daf9ce497ee0702131cf7d8f
faizalajiw <faizalajiwibowo7@gmail.com>
2023-05-14 17:31:44 +0700

    card saldo akhir

--- application/views/laporan/rekap.php
+++ application/views/laporan/rekap.php
@@ -8,67 +8,98 @@
 		<div class="col-12">
 			<div class="card my-4">
 				<div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-					<div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+					<div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3 mb-2">
 						<h6 class="text-white text-capitalize ps-3">Rekap Laporan</h6>
+					</div>
+					<hr class="dark horizontal my-3">
+					<div class="row">
+						<div class="card col-4">
+							<div class="card-header p-3 pt-2">
+								<div class="icon icon-lg icon-shape bg-gradient-info shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+									<i class="material-icons opacity-10" translate="no">table_view</i>
+								</div>
+								<div class="text-start pt-6">
+									<p class="text-2xl text-bold mb-0">Total Pemasukan</p>
+									<h4 class="mb-0">Rp.
+										<!-- <?= number_format($pendapatan['pendapatan_sekarang']) ?> -->
+									</h4>
+								</div>
+							</div>
+						</div>
+						<div class="card col-4">
+							<div class="card-header p-3 pt-2">
+								<div class="icon icon-lg icon-shape bg-gradient-info shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+									<i class="material-icons opacity-10" translate="no">table_view</i>
+								</div>
+								<div class="text-start pt-6">
+									<p class="text-2xl text-bold mb-0">Total Pengeluaran</p>
+									<h4 class="mb-0">Rp.
+										<!-- <?= number_format($pendapatan['pendapatan_sekarang']) ?> -->
+									</h4>
+								</div>
+							</div>
+						</div>
+						<div class="card col-4">
+							<div class="card-header p-3 pt-2">
+								<div class="icon icon-lg icon-shape bg-gradient-info shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+									<i class="material-icons opacity-10" translate="no">table_view</i>
+								</div>
+								<div class="text-start pt-6">
+									<p class="text-2xl text-bold mb-0">Saldo Akhir</p>
+									<h4 class="mb-0">Rp.
+										<!-- <?= number_format($pendapatan['pendapatan_sekarang']) ?> -->
+									</h4>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div class="card-body px-0 pb-2">
-					<!-- <div class="text-end me-3">
-						<button type="button" class="btn btn-secondary" data-bs-toggle="modal"
-							data-bs-target="#modalTambah">
-							<i class="material-icons opacity-10" translate="no">add</i>Cetak
-						</button>
-					</div> -->
 					<div class="table-responsive p-4 mx-2">
 						<table class="table align-items-center mb-0" id="datatable">
 							<thead>
 								<tr>
-									<th
-										class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+									<th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
 										Tanggal
-                                    </th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Kategori
-                                    </th>
+									</th>
+									<th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+										Kategori
+									</th>
 									<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
 										Keterangan
 									</th>
-									<th
-										class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+									<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
 										Pemasukan
-                                    </th>
-									<th
-										class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+									</th>
+									<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
 										Pengeluaran
-                                    </th>
+									</th>
 								</tr>
 							</thead>
 							<tbody>
-								<?php foreach($result as $row) : ?>
-								<tr>
-									<td class="align-middle text-center">
-										<span
-											class="text-secondary text-sm font-weight-bold"><?= $row->tanggal ?>
-                                        </span>
-									</td>
-                                    <td class="align-middle text-center text-sm">
-										<span>
-											<?= $row->kategori ?>
-										</span>
-									</td>
-									<td>
-										<p class="text-sm text-secondary mb-0"><?= $row->keterangan ?></p>
-									</td>
-									<td>
-										<p class="text-sm font-weight-bold mb-0">Rp. <?= number_format($row->jumlah_masuk) ?>
-										</p>
-									</td>
-									<td>
-										<p class="text-sm font-weight-bold mb-0">Rp. <?= number_format($row->jumlah_keluar) ?>
-										</p>
-									</td>
-								</tr>
+								<?php foreach ($result as $row) : ?>
+									<tr>
+										<td class="align-middle text-center">
+											<span class="text-secondary text-sm font-weight-bold"><?= $row->tanggal ?>
+											</span>
+										</td>
+										<td class="align-middle text-center text-sm">
+											<span>
+												<?= $row->kategori ?>
+											</span>
+										</td>
+										<td>
+											<p class="text-sm text-secondary mb-0"><?= $row->keterangan ?></p>
+										</td>
+										<td>
+											<p class="text-sm font-weight-bold mb-0">Rp. <?= number_format($row->jumlah_masuk) ?>
+											</p>
+										</td>
+										<td>
+											<p class="text-sm font-weight-bold mb-0">Rp. <?= number_format($row->jumlah_keluar) ?>
+											</p>
+										</td>
+									</tr>
 								<?php endforeach ?>
 							</tbody>
 						</table>
@@ -80,17 +111,17 @@
 </div>
 
 <script>
-	$(document).ready( function () {
+	$(document).ready(function() {
 		$('#datatable').DataTable({
 			language: {
 				"paginate": {
-					"first":      "&laquo",
-					"last":       "&raquo",
-					"next":       "&gt",
-					"previous":   "&lt"
+					"first": "&laquo",
+					"last": "&raquo",
+					"next": "&gt",
+					"previous": "&lt"
 				},
 			},
-			dom:' <"d-flex"l<"input-group input-group-outline justify-content-end me-4"f>>rt<"d-flex justify-content-between"ip><"clear">'
+			dom: ' <"d-flex"l<"input-group input-group-outline justify-content-end me-4"f>>rt<"d-flex justify-content-between"ip><"clear">'
 		});
-	} );
+	});
 </script>
