4f6d737330d1f2b8f19771fbfd7fa8f7534cdfc9
faizalajiw <faizalajiwibowo7@gmail.com>
2023-05-16 13:57:48 +0700

    fix tampilan pdf

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -14,11 +14,10 @@ class Laporan extends CI_Controller
             $pecahTanggal = explode(' - ', $tanggal);
             $tglMulai = date('Y-m-d', strtotime($pecahTanggal[0]));
             $tglAkhir = date('Y-m-d', strtotime(end($pecahTanggal)));
-            
+
             $query = $this->histori_m->getLaporan($table, ['mulai' => $tglMulai, 'akhir' => $tglAkhir]);
             $this->_cetak($query, $table, $tanggal);
-
-        }else{
+        } else {
             $this->template->load('template/template', 'laporan/laporan');
         }
     }
@@ -32,7 +31,7 @@ class Laporan extends CI_Controller
         $pdf->AddPage('P', 'Letter');
         // $pdf->Image('logo.png',20,6,23);
         $pdf->SetFont('Arial', 'B', 10);
-        $pdf->Cell(190, 7, '', 0, 1, 'C');
+        $pdf->Cell(190, 7, '', 0, 1, 'L');
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(190, 7, 'IDI Cabang Brebes', 0, 1, 'C');
         $pdf->SetFont('Arial', 'B', 8);
@@ -48,40 +47,84 @@ class Laporan extends CI_Controller
 
         $pdf->SetFont('Arial', 'B', 9);
 
-        $total = 0;
+        $total_masuk = 0;
+        $total_keluar = 0;
         if ($table == 'kas') :
-            
-            $pdf->Cell(10, 7, 'No.', 1, 0, 'C');
-            $pdf->Cell(20, 7, 'Tanggal', 1, 0, 'C');
-            $pdf->Cell(77, 7, 'Keterangan', 1, 0, 'C');
-            $pdf->Cell(38, 7, 'Kategori', 1, 0, 'C');
-            $pdf->Cell(25, 7, 'Jumlah Masuk', 1, 0, 'C');
-            $pdf->Cell(25, 7, 'Jumlah Keluar', 1, 0, 'C');
-            $pdf->Ln();
+        // ------------------ KAS MASUK --------------------
+            // Title 1: Kas Masuk
+            $pdf->SetFont('Arial', 'B', 12);
+            $pdf->Cell(0, 10, 'Kas Masuk', 0, 1, 'L');
+            $pdf->Ln(3);
 
-            $no = 1;
+            // Table header
+            $pdf->SetFont('Arial', 'B', 10);
+            $pdf->Cell(30, 7, 'Tanggal', 'B', 0, 'C');
+            $pdf->Cell(100, 7, 'Keterangan', 'B', 0, 'C');
+            $pdf->Cell(65, 7, 'Jumlah Masuk', 'B', 1, 'C');
+
+            // Table rows
+            $pdf->SetFont('Arial', '', 10);
             foreach ($data as $d) {
-                $pdf->SetFont('Arial', '', 8);
-                $pdf->Cell(10, 7, $no++ . '.', 1, 0, 'C');
-                $pdf->Cell(20, 7, $d->tanggal, 1, 0, 'C');
-                $pdf->Cell(77, 7, $d->keterangan, 1, 0, 'L');
-                $pdf->Cell(38, 7, $d->kategori, 1, 0, 'C');
+                $total_masuk += $d->jumlah_masuk;
+
                 if ($d->jumlah_masuk != 0) {
-                    $pdf->Cell(25, 7,"Rp. " . number_format($d->jumlah_masuk), 1, 0, 'L');
+                    $pdf->Cell(30, 7, $d->tanggal, 0, 0, 'L');
+                    $pdf->Cell(100, 7, $d->keterangan, 0, 0, 'L');
+                    $pdf->Cell(65, 7, "Rp. " . number_format($d->jumlah_masuk), 0, 1, 'C');
                 } else {
-                    $pdf->Cell(25, 7, "-", 1, 0, 'C');
+                    // $pdf->Cell(65, 7, "-", 0, 1, 'C');
                 }
-                if ($d->jumlah_keluar != 0) {
-                    $pdf->Cell(25, 7,"Rp. " . number_format($d->jumlah_keluar), 1, 0, 'L');
-                } else {
-                    $pdf->Cell(25, 7, "-", 1, 0, 'C');
-                }
-                $pdf->Ln();
-                $total += $d->jumlah_masuk - $d->jumlah_keluar;
             }
-            $pdf->Cell(145, 7, 'Jumlah', 1, 0, 'C');
-            $pdf->Cell(50, 7, "Rp. " . number_format($total), 1, 0, 'C');
-            $pdf->Ln();
+            $pdf->Ln(3);
+            // Total Masuk
+            $pdf->SetFont('Arial', 'B', 10);
+            $pdf->Cell(130, 7, 'Total Pemasukan :', 0, 0, 'R');
+            $pdf->Cell(65, 7, "Rp. " . number_format($total_masuk), 0, 1, 'C');
+            $pdf->Ln(10);
+        // ------------------ KAS MASUK --------------------
+
+        // ------------------ KAS KELUAR --------------------
+            // Title 1: Kas Keluar
+            $pdf->SetFont('Arial', 'B', 12);
+            $pdf->Cell(0, 10, 'Kas Keluar', 0, 1, 'L');
+            $pdf->Ln(3);
+
+            // Table header
+            $pdf->SetFont('Arial', 'B', 10);
+            $pdf->Cell(30, 7, 'Tanggal', 'B', 0, 'C');
+            $pdf->Cell(100, 7, 'Keterangan', 'B', 0, 'C');
+            $pdf->Cell(65, 7, 'Jumlah Keluar', 'B', 1, 'C');
+
+            // Table rows
+            $pdf->SetFont('Arial', '', 10);
+            foreach ($data as $d) {
+                $total_keluar += $d->jumlah_keluar;
+
+                if ($d->jumlah_keluar != 0) {
+                    $pdf->Cell(30, 7, $d->tanggal, 0, 0, 'L');
+                    $pdf->Cell(100, 7, $d->keterangan, 0, 0, 'L');
+                    $pdf->Cell(65, 7, "Rp. " . number_format($d->jumlah_keluar), 0, 1, 'C');
+                } else {
+                    // $pdf->Cell(65, 7, "-", 0, 1, 'C');
+                }
+            }
+            $pdf->Ln(3);
+            // Total Keluar
+            $pdf->SetFont('Arial', 'B', 10);
+            $pdf->Cell(130, 7, 'Total Pengeluaran :', 0, 0, 'R');
+            $pdf->Cell(65, 7, "Rp. " . number_format($total_keluar), 0, 1, 'C');
+            $pdf->Ln(10);
+        // ------------------ KAS KELUAR --------------------
+
+        // ------------------ SALDO AKHIR -------------------
+            // Calculate Saldo Akhir
+            $saldo_akhir = $total_masuk - $total_keluar;
+
+            // Title 3: Saldo Akhir
+            $pdf->SetFont('Arial', 'B', 12);
+            $pdf->Cell(30, 10, 'Saldo Akhir : ', 0, 0, 'L');
+            $pdf->Cell(70, 10, "Rp. " . number_format($saldo_akhir), 0, 0, 'L');
+        // ------------------ SALDO AKHIR -------------------
         endif;
 
 
